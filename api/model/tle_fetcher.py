b76356be59877f99a08be9884a1cff302b1ef3ca
alaa <alaamglal@gmail.com>
2024-07-10 01:42:45 -0700

    done

--- api/model/tle_fetcher.py
+++ api/model/tle_fetcher.py
@@ -50,13 +50,13 @@ class Base:
         line1 = tle_data[1].strip()
         line2 = tle_data[2].strip()
         satellite = Satrec.twoline2rv(line1, line2)
-        if date_time:
-            local_zone = timezone('Africa/Cairo')
+        local_zone = timezone('Africa/Cairo')
+        if self.date_time:
             date_time = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
             local_date_time = local_zone.localize(date_time)
             utc_time = local_date_time.astimezone(timezone('UTC'))
         else:
-            utc_time = timezone('UTC')
+            utc_time = datetime.now(timezone('UTC'))
 
         year, month, day, hour, minute, sec = (
                 utc_time.year, utc_time.month,
@@ -66,12 +66,12 @@ class Base:
         julien_day, fr = jday(year, month, day, hour, minute, sec)
         e, r, v = satellite.sgp4(julien_day, fr)
         if e == 0:
-            return({
-                "date": utc_time.date()
-                "time": utc_time.time()
+            return ({
+                "a_date": str(utc_time.date()),
+                "a_time": str(utc_time.time()),
+                "name": self.sat_name,
                 "r_vector": {"x": r[0], "y": r[1], "z": r[2]},
                 "v_vector": {"x": v[0], "y": v[1], "z": v[2]}
-                "name": self.sat_name
                 })
         else:
             raise RuntimeError("Error")
--- api/route/satellite.py
+++ api/route/satellite.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from api.route import main
 from api.model.tle_fetcher import Base
-from flask import abort, jsonify
+from flask import abort, jsonify, request
 
 @main.route("/active_sat/<name>", methods=["GET"], strict_slashes=False)
 def get_sat(name):
@@ -16,3 +16,25 @@ def get_sat(name):
     tle_data = satellite.get_tle(norad_id)
     rv_data = satellite.predict_rv(tle_data)
     return(jsonify(rv_data))
+
+@main.route("/active_sat/<name>", methods=["PUT"], strict_slashes=False)
+def update_sat(name):
+    """
+    Update datetime of position and velocity vector
+    """
+    satellite = Base(name)
+    norad_id = satellite.get_norad_id()
+    tle_data = satellite.get_tle(norad_id)
+    if name:
+        if not request.get_json():
+            abort(400, description="Not a JSON")
+
+        data = request.get_json()
+        ignore = ["r_vector", "v_vector", "name", "date_time"]
+        for key, value in data.items():
+            if key not in ignore:
+                setattr(satellite, key, value)
+        rv_data = satellite.predict_rv(tle_data)
+        return (jsonify(rv_data))
+    else:
+        abort(404)
