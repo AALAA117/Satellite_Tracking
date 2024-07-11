c9940ca85d2e9c71c8feec66dfebe17a11178769
alaa <alaamglal@gmail.com>
2024-07-11 09:07:14 +0000

    done

--- run.py
+++ run.py
@@ -29,4 +29,4 @@ if __name__ == '__main__':
         host = '0.0.0.0'
     if not port:
         port = 5005
-    app.run(debug=True, host=host, port=port)
+    app.run(debug=False, host=host, port=port)
